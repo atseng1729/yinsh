1075dd7d7bf3c76281f05f362867027c43d80cd6
Arthur Tseng <atseng1729@gmail.com>
2020-06-02 16:52:57 -0400

    code for basic board layout

---
+++ boardLayout.py
@@ -0,0 +1,46 @@
+import itertools
+import math
+
+side = 50
+# -30 degrees and +90 degrees for x and y axial unit vectors
+radian_x = - math.pi / 6
+radian_y = math.pi / 2
+# cartesian coordinates for the x and y axial unit vectors
+cart_x = [side * math.cos(radian_x), side * math.sin(radian_x)]
+cart_y = [side * math.cos(radian_y), side * math.sin(radian_y)]
+
+def axial_to_cartesian(v):
+    return (round(v[0] * cart_x[0] + v[1] * cart_y[0], 2),
+            round(v[0] * cart_x[1] + v[1] * cart_y[1], 2))
+
+def euclidean_distance(v1, v2):
+    return math.sqrt((v1[0] - v2[0])*(v1[0] - v2[0]) + (v1[1] - v2[1])*(v1[1] - v2[1]))
+
+def get_axial_vertices():
+    vertices = ([(-5, i) for i in range(-4, 0)]
+             + [(-4, i) for i in range(-5, 2)]
+             + [(-3, i) for i in range(-5, 3)]
+             + [(-2, i) for i in range(-5, 4)]
+             + [(-1, i) for i in range(-5, 5)]
+             + [(0, i) for i in range(-4, 5)]
+             + [(1, i) for i in range(-4, 6)]
+             + [(2, i) for i in range(-3, 6)]
+             + [(3, i) for i in range(-2, 6)]
+             + [(4, i) for i in range(-1, 6)]
+             + [(5, i) for i in range(1, 5)])
+    return vertices
+
+def get_cartesian_vertices(vertices):
+    return list(map(axial_to_cartesian, vertices))
+
+def get_edges(cart_vertices):
+    edges = []
+    for v1, v2 in itertools.permutations(cart_vertices, 2):
+        if euclidean_distance(v1, v2) <= side + 0.1:
+            edges.append((v1, v2))
+    return edges
+
+if __name__=="__main__":
+    axial_vertices = get_axial_vertices()
+    cart_vertices = get_cartesian_vertices(axial_vertices)
+    edges = get_edges(cart_vertices)
---
+++ src/Yinsh.elm
@@ -0,0 +1,60 @@
+module Yinsh exposing (main)
+
+import Browser
+import Html exposing (..)
+import Html.Attributes as Attr
+import Debug
+import Time
+import Collage exposing (segment, traced, solid, thin, uniform, Point)
+import Collage.Layout exposing (stack)
+import Collage.Render exposing (svg)
+import Color
+
+import Constants exposing (vertices, edges)
+
+main : Program Flags Model Msg
+main =
+  Browser.element
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+type alias Model = {}
+type alias Flags = ()
+type Msg = Tick Time.Posix
+
+initModel : Model
+initModel = {}
+
+init : Flags -> (Model, Cmd Msg)
+init () =
+  (initModel, Cmd.none)
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Time.every 10 Tick
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  (model, Cmd.none)
+
+renderBoard : List (Point, Point) -> Html msg
+renderBoard edges =
+  List.map (\(p1, p2) -> segment p1 p2 |> traced (solid thin (uniform Color.grey))) edges
+    |> stack
+    |> svg
+
+view : Model -> Html Msg
+view model =
+  let
+    board = renderBoard edges 
+    styles =
+      [ ("position", "fixed")
+      , ("top", "50%")
+      , ("left", "50%")
+      , ("transform", "translate(-50%, -50%)")
+      ]
+  in
+    div (List.map (\(k, v) -> Attr.style k v) styles) [ board ]
